d978082549ad9675ddf22b68c160e8008dda5d1a
Thomas Smith <smithtg@ncbi.nlm.nih.gov>
2016-12-13 15:51:00 -0500

    Very basic hypothesis tests

--- test.py
+++ test.py
@@ -1,14 +1,41 @@
 import io
 import json
+import math
 
 from hypothesis import strategies as st, given
 
 import jsonfile
 
 
-@given(st.text())
+# Generate lists and dicts with various values.
+# See http://hypothesis.readthedocs.io/en/latest/data.html#recursive-data
+json_objects = st.recursive(
+    st.one_of(
+        st.text(),
+        st.floats().filter(lambda f: not math.isnan(f)),
+        st.booleans(),
+        st.none()),
+    lambda children: st.one_of(
+        st.lists(children),
+        st.dictionaries(st.text(), children)))
+# NaN: serializing and deserializing work fine, but it's a pain to compare
+# containers for equality when they contain NaN.
+
+
+@given(json_objects)
 def test_write_obj(obj):
     out = io.StringIO()
     f = jsonfile.JsonWriter(out)
     f.write_obj(obj)
-    assert json.loads(out.getvalue()) == obj
+
+    remade = json.loads(out.getvalue())
+    assert remade == obj
+
+
+def test_write_nan():
+    out = io.StringIO()
+    f = jsonfile.JsonWriter(out)
+    f.write_obj(float('nan'))
+
+    remade = json.loads(out.getvalue())
+    assert math.isnan(remade)
